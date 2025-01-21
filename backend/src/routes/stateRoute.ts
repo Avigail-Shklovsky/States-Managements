883e588143ad6a8a77edecee8952f21eaafa9e40
Avigail Shklovsky <9013825@gmail.com>
2025-01-21 16:05:15 +0200

    add api route

--- backend/src/routes/stateRoute.ts
+++ backend/src/routes/stateRoute.ts
@@ -1,12 +1,21 @@
-import express from 'express';
-import { createState, getStates, updateState, deleteState, getStateById } from '../controllers/stateController';
+import express from "express";
+import {
+  createState,
+  getStates,
+  updateState,
+  deleteState,
+  getStateById,
+} from "../controllers/stateController";
+import initializeData from "../services/fetchStatesData";
 
 const stateRoutes = express.Router();
 
-stateRoutes.post('/', createState);
-stateRoutes.get('/', getStates);
-stateRoutes.get('/:id', getStateById);
-stateRoutes.put('/:id', updateState);
-stateRoutes.delete('/:id', deleteState);
+stateRoutes.post("/", createState);
+stateRoutes.get("/", getStates);
+stateRoutes.get("/:id", getStateById);
+stateRoutes.put("/:id", updateState);
+stateRoutes.delete("/:id", deleteState);
+stateRoutes.get("/initialDB", initializeData);
+
 
 export default stateRoutes;
--- backend/src/services/fetchStatesData.ts
+++ backend/src/services/fetchStatesData.ts
@@ -1,9 +1,7 @@
-import axios from "axios";
 import StateModel from "../models/state";
+import { fetchStatesData } from "../utils/fetchStatesData";
 
-
-const fetchStatesData = async () => {
-
+const initializeData = async () => {
   try {
     // Check if the StateModel collection is empty
     const count = await StateModel.countDocuments();
@@ -13,35 +11,12 @@ const fetchStatesData = async () => {
       console.log("Data already exists in the database. No need to fetch data");
       return;
     }
-
   } catch (error) {
-    return; 
+    return;
   }
 
   // Fetch data from API
-  try {
-    console.log("getting data");
-         
-    const response = await axios.get("https://restcountries.com/v3.1/all");
-
-    if (response) {
-      const states = response.data;
-      // Map and transform data if needed
-      const extractedStates = states.map((state: any) => ({
-        name: state.name.common,
-        flag: state.flags.png,
-        population: state.population,
-        region: state.region,
-      }));
-
-      // Insert data into MongoDB
-      await StateModel.insertMany(extractedStates);
-
-      console.log("Data successfully saved to MongoDB.");
-    }
-  } catch (error) {
-    console.error("Error fetching data from the API:", error);
-  }
+  fetchStatesData();
 };
 
-export default fetchStatesData;
+export default initializeData;
